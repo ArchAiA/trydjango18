f854ba44ee6f9ea2ceb2f0d1b100078c7b2a0cc3
david <xarchaiax@gmail.com>
2016-01-24 19:16:09 -0500

    Lesson 13

--- newsletter/forms.py
+++ newsletter/forms.py
@@ -4,5 +4,19 @@ from .models import SignUp
 class SignUpForm(forms.ModelForm):
 	class Meta:
 		model = SignUp
-		fields = ['email','full_name']
+		fields = ['full_name', 'email']
 		#exclude = ['full_name'] #Use Sparingly
+
+#Over-riding the function to clean data
+	def clean_email(self):
+		email = self.cleaned_data.get('email')
+		email_base, provider = email.split("@")
+		domain, extension = provider.split(".")
+		if not extension == "edu":
+			raise forms.ValidationError("Please use a valid .edu email address")
+		return email
+
+	def clean_full_name(self):
+		full_name = self.cleaned_data.get("full_name")
+		return full_name 
+
--- newsletter/views.py
+++ newsletter/views.py
@@ -2,5 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
+	title = 'Welcome'
+	if request.user.is_authenticated():
+		title = "My Title %s" % (request.user)
+	
+	context = {
+		"template_title": title,
+		}
 
-	return render(request, "home.html", {})
+	return render(request, "home.html", context) #Render comibines and returns (Request, Template, and Context)
